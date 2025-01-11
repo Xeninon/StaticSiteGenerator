e4e80022703c4f667af2643ce371de9fe5dd4e6a
Xeninon <alecdscout@gmail.com>
2025-01-10 21:38:51 -0600

    can split text nodes on delimiter

--- src/convert.py
+++ src/convert.py
@@ -18,3 +18,19 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("invalid text type")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            text_split = node.text.split(delimiter)
+            if len(text_split) % 2 < 0 or len(text_split) < 3:
+                raise Exception("invalid_syntax")
+            for text in text_split:
+                if text_split.index(text) % 2 == 1:
+                    new_nodes.append(TextNode(text, text_type))
+                else:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+        else:
+            new_nodes.append(node)
+    return new_nodes
--- src/test_convert.py
+++ src/test_convert.py
@@ -25,3 +25,20 @@ class Test_text_node_to_html_node(unittest.TestCase):
         answer_key = zip(test_inputs, correct_outputs)
         for key in answer_key:
             self.assertEqual(text_node_to_html_node(key[0]).__repr__(), key[1].__repr__())
+
+
+class Test_split_nodes_delimiter(unittest.TestCase):
+    def test(self):
+        test_inputs = [
+            ([TextNode("**hello** world", TextType.TEXT),TextNode("hello world", TextType.BOLD)], "**", TextType.BOLD),
+            ([TextNode("**hello world**", TextType.TEXT)], "**", TextType.BOLD),
+        ]
+
+        correct_outputs = [
+            [TextNode("", TextType.TEXT), TextNode("hello", TextType.BOLD), TextNode(" world", TextType.TEXT), TextNode("hello world", TextType.BOLD)],
+            [TextNode("", TextType.TEXT), TextNode("hello world", TextType.BOLD), TextNode("", TextType.TEXT)],
+        ]
+
+        answer_key = zip(test_inputs, correct_outputs)
+        for key in answer_key:
+            self.assertEqual(split_nodes_delimiter(*key[0]), key[1])
